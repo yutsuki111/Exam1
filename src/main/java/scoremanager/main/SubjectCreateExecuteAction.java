7d8756ffa76210d28d7a46156236c3efec58196f
s_morinaga <s_morinaga@LAPTOP-5Q76KTBF>
2026-04-16 11:37:35 +0900

    touroku

--- src/main/java/scoremanager/main/SubjectCreateExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectCreateExecuteAction.java
@@ -1,5 +1,6 @@
 package scoremanager.main;
 
+import bean.School;
 import bean.Subject;
 import bean.Teacher;
 import dao.SubjectDao;
@@ -12,15 +13,30 @@ public class SubjectCreateExecuteAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res)
 	throws Exception{
-		HttpSession session = req.getSession(); 
+		HttpSession session = req.getSession();
 		Teacher teacher = (Teacher)session.getAttribute("user");
 		String cd = req.getParameter("cd");
 		String name = req.getParameter("name");
 		Subject subject = new Subject();
+		School school = teacher.getSchool();
 		SubjectDao subDao = new SubjectDao();
+		if(cd.length() < 3 ) {
+			req.setAttribute("errortext", "科目コードは3文字で入力してください");
+			req.getRequestDispatcher("subject_create.jsp").forward(req, res);
+			return;
+		}
+		subject = subDao.get(cd, school);
+		if(subject != null) {
+			req.setAttribute("errortext", "科目コードが重複しています");
+			req.getRequestDispatcher("subject_create.jsp").forward(req, res);
+			return;
+		}
+		subject = new Subject();
+		subject.setCd(cd);
+		subject.setName(name);
+		subject.setSchool(school);
 		
-		
-		
+		 subDao.save(subject);
 		
 		
 		req.getRequestDispatcher("subject_create_done.jsp").forward(req, res);
