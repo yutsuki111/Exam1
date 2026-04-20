8bf420bc0ff744a039457ffdd9d30fb80c638f3a
s_morinaga <s_morinaga@LAPTOP-5Q76KTBF>
2026-04-20 10:21:08 +0900

    コメント

--- src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -14,25 +14,29 @@ public class SubjectDeleteExecuteAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-		// 1. リクエストパラメータの取得
+		// リクエストパラメータの取得
 		String cd = req.getParameter("subject_cd");
 		String name = req.getParameter("subject_name");
+		// セッションを取得
 		HttpSession session = req.getSession(); 
+		// ログイン中のTeacher情報を取得
 		Teacher teacher = (Teacher)session.getAttribute("user"); 
+		// ログイン中のteacherが所属しているSchoolを取り出す
 		School school = teacher.getSchool();
+		// インスタンス化
 		Subject subject = new Subject();
 		
-		// 3. 更新用データの作成
+		// 更新用データの作成
 		subject.setCd(cd);
 		subject.setName(name);
 		subject.setSchool(school);
 
-		// 4. DB更新実行
+		// DB更新実行
 		SubjectDao subDao = new SubjectDao();
 		// メソッドは、内部でgetして存在すればUPDATEを実行する仕様なのでこれだけでOK
 		subDao.delete(subject);
 
-		// 5. 完了画面へフォワード
+		// subject_delete_done.jspへフォワード
 		req.getRequestDispatcher("subject_delete_done.jsp").forward(req, res);
 	}
 }
--- src/main/java/scoremanager/main/SubjectListAction.java
+++ src/main/java/scoremanager/main/SubjectListAction.java
@@ -15,15 +15,21 @@ public class SubjectListAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res)
 	throws Exception{
+		// セッションを取得
 		HttpSession session = req.getSession(); 
+		// ログイン中のTeacher情報を取得
 		Teacher teacher = (Teacher)session.getAttribute("user"); 
+		// ログイン中のteacherが所属しているSchoolを取り出す
 		School school = teacher.getSchool();
-		 
-		List<Subject> subjects; 
+		// リスト作成
+		List<Subject> subjects;
+		// インスタンス化
 		SubjectDao subDao = new SubjectDao();
+		// filterメソッドを呼び出し
 		subjects = subDao.filter(school);
-		
+		// リクエストにデータをセット
 		req.setAttribute("subjects", subjects); 
+		// subject_list.jspへフォア―ド
 		req.getRequestDispatcher("subject_list.jsp").forward(req, res);
 	}
 }
