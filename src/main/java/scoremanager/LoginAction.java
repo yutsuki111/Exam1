e9661befd5348baf81308a40ebbdb0aaaec83ed8
s_morinaga <s_morinaga@LAPTOP-5Q76KTBF>
2026-04-24 10:47:36 +0900

    s

--- src/main/java/scoremanager/LoginAction.java
+++ src/main/java/scoremanager/LoginAction.java
@@ -2,21 +2,16 @@ package scoremanager;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
 public class LoginAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res)
 	throws Exception{
-		HttpSession session = req.getSession(false);
-	    if (session == null || session.getAttribute("user") == null) {
-	        res.sendRedirect("Login.action");
-	        return;
-	    }else {
-	    	// login.jspにフォア―ド
-			req.getRequestDispatcher("login.jsp").forward(req, res);
-	    }
+
+	// login.jspにフォア―ド
+	req.getRequestDispatcher("login.jsp").forward(req, res);
+
 		
 	}
 }
