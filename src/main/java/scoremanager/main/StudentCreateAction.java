32db25883286498dba4dd889f12685f28b7f65b6
y_niiwa <y_niiwa@LAPTOP-VC6TH8I0>
2026-04-24 11:20:18 +0900

    asdfgh

--- src/main/java/scoremanager/main/StudentCreateAction.java
+++ src/main/java/scoremanager/main/StudentCreateAction.java
@@ -4,43 +4,40 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import bean.School;
+import bean.Teacher;
 import dao.ClassNumDao;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import tool.Action;
-
-
 
 public class StudentCreateAction extends Action {
 
-	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-		// HttpSession session = req.getSession(); // 本来のログイン処理
-		// Teacher teacher = (Teacher)session.getAttribute("user");
+        HttpSession session = req.getSession(); // 本来のログイン処理
+        Teacher teacher = (Teacher)session.getAttribute("user");
 
-		// 【テスト用】仮の学校情報を設定
-		School school = new School();
-		school.setCd("oom");
+        // 入学年度のプルダウンリストを作成（10年前から今年まで）
+        LocalDate todaysDate = LocalDate.now();
+        int year = todaysDate.getYear();
+        List<Integer> entYearSet = new ArrayList<>();
+        for (int i = year - 10; i <= year; i++) {
+            entYearSet.add(i);
+        }
 
-		// 1. 入学年度のプルダウン用リストを作成（今年から10年前まで）
-		LocalDate todaysDate = LocalDate.now();
-		int year = todaysDate.getYear();
-		List<Integer> entYearSet = new ArrayList<>();
-		for (int i = year - 10; i <= year; i++) {
-			entYearSet.add(i);
-		}
+        // ラス番号のプルダウンリストをDBから取得
+        ClassNumDao classNumDao = new ClassNumDao();
+        
+        List<String> list = classNumDao.filter(teacher.getSchool());
 
-		// 2. クラス番号のプルダウン用リストをDBから取得
-		ClassNumDao classNumDao = new ClassNumDao();
-		List<String> list = classNumDao.filter(school);
+        // 画面（JSP）に渡すデータをリクエストにセット
+        req.setAttribute("ent_year_set", entYearSet); // 入学年度の選択肢
+        
+        req.setAttribute("class_num_set", list);      // クラスの選択肢
 
-		// 3. 画面（JSP）に渡すデータをリクエストにセット
-		req.setAttribute("ent_year_set", entYearSet); // 入学年度の選択肢
-		req.setAttribute("class_num_set", list);      // クラスの選択肢
-
-		// 4. 学生登録画面（JSP）へフォワード
-		req.getRequestDispatcher("student_create.jsp").forward(req, res);
-	}
+        // 学生登録画面（JSP）へフォワード
+        req.getRequestDispatcher("student_create.jsp").forward(req, res);
+    }
 }
