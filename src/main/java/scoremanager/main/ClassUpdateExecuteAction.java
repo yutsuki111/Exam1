0f23184be5cc7c4c6bee295f64ee01b6a1ac69c2
s_morinaga <s_morinaga@LAPTOP-5Q76KTBF>
2026-05-08 10:08:05 +0900

    s

--- src/main/java/scoremanager/main/ClassUpdateExecuteAction.java
+++ src/main/java/scoremanager/main/ClassUpdateExecuteAction.java
@@ -27,15 +27,28 @@ public class ClassUpdateExecuteAction extends Action {
 		
 		// インスタンス化
 		ClassNum num = new ClassNum();
-		
+		// DB更新実行
+		ClassNumDao numDao = new ClassNumDao();
+		// 重複がないかgetメソッドを呼び出し
+		ClassNum check = numDao.get(class_num, school);
+		// 重複してる時
+		if(check != null && !class_num.equals(old_class_num)) {
+			// リクエストにデータをセット
+			req.setAttribute("errortext", "クラス名が重複しています");
+			num.setClass_num(class_num);
+			req.setAttribute("num", num);
+			// ubject_create.jspにフォア―ド
+			req.getRequestDispatcher("class_update.jsp").forward(req, res);
+			return;
+		}
 		// 更新用データの作成
 		num.setClass_num(class_num);
 		num.setSchool(school);
 		
-		// DB更新実行
-		ClassNumDao numDao = new ClassNumDao();
+		
 		// StudentDaoのsaveメソッドは、内部でgetして存在すればUPDATEを実行する仕様なのでこれだけでOK
 		boolean result = numDao.save(num, old_class_num);
+		
 		// フラグがない場合
 		if(!result) {
 			// リクエストデータにセット
